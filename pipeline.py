72480a2fc3a8d14101e1e3dfe1e3449ce7a6e2d0
Muhammad Amin Khan <adrishak8823813@gmail.com>
2025-08-14 13:55:22 +0500

    Add mock pipeline script

---
+++ pipeline.py
@@ -0,0 +1,5 @@
+# Mock GRASS-GEE-SWMM pipeline
+print("Downloading DEM from GEE... (mock)")
+print("Processing in GRASS GIS... (mock)")
+print("Creating SWMM input file... (mock)")
+print("Pipeline complete!")
